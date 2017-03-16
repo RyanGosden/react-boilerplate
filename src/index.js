996d58a2b48448eb1b2eebc52337aa182422746b
Ryan Gosden <ryangosden@gmail.com>
2017-03-16 09:43:12 +0100

    added antd

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-alert("Compiled!!!");
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+export default class x extends React.Component {
+
+    render(){
+        return(
+            <div>
+                Hi
+            </div>
+        );
+    }
+
+}
+
+
+reactDOM.render(<x/>, document.getElementById('root'));
